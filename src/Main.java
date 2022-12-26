cca3f54ec948078ba91ada9244d92750ee552428
SergeiAnishchenko <79313219184@yandex.ru>
2022-12-26 20:06:44 +0300

    Домашка по уроку 9.Строки.

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        System.out.println(" ");
+        task2();
+        System.out.println(" ");
+        task3();
+    }
+
+    private static void task1() {
+        System.out.println("Задача 1");
+    }
+
+    private static void task2() {
+        System.out.println("Задача 2");
+    }
+
+    private static void task3() {
+        System.out.println("Задача 3");
+    }
+}
